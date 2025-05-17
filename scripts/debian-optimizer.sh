2a19c9ecd19f3ff4571ce48200ffe4ad62b06bd6
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2025-05-17 03:35:20 +0000

    Update debian-optimizer.sh

--- scripts/debian-optimizer.sh
+++ scripts/debian-optimizer.sh
@@ -372,13 +372,9 @@ net.core.rmem_max = 16777216
 net.core.wmem_max = 16777216
 net.ipv4.tcp_rmem = 4096 87380 16777216
 net.ipv4.tcp_wmem = 4096 65536 16777216
-net.ipv4.tcp_window_scaling = 1
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_sack = 1
 net.ipv4.tcp_fastopen = 0
-net.core.netdev_max_backlog = 5000
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_fin_timeout = 15
 net.ipv4.tcp_tw_reuse = 1
 
 ################################################################
