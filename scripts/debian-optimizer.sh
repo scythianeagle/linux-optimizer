5064fe07113a9a8a3bd99e04a52434b49f657901
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2025-08-01 09:24:10 +0000

    Update debian-optimizer.sh

--- scripts/debian-optimizer.sh
+++ scripts/debian-optimizer.sh
@@ -326,49 +326,31 @@ cat <<EOF >> "$SYS_PATH"
 # These parameters in this file will be added/updated to the sysctl.conf file.
 # Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
 
-fs.file-max = 67108864
-net.core.netdev_max_backlog = 32768
-net.core.optmem_max = 262144
-net.core.somaxconn = 65536
-net.core.rmem_max = 33554432
-net.core.rmem_default = 1048576
-net.core.wmem_max = 33554432
-net.core.wmem_default = 1048576
-net.ipv4.ipfrag_high_thresh = 524288
-net.ipv4.ipfrag_low_thresh = 446464
-net.ipv4.ipfrag_time = 60
-vm.dirty_background_ratio = 5
-vm.dirty_expire_centisecs = 3000
-vm.dirty_writeback_centisecs = 500
-net.ipv4.tcp_rmem = 16384 1048576 33554432
-net.ipv4.tcp_wmem = 16384 1048576 33554432
-net.ipv4.tcp_fin_timeout = 25
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.tcp_keepalive_probes = 7
+net.core.rmem_max = 256960
+net.core.rmem_default = 256960
+net.core.wmem_max = 256960
+net.core.wmem_default = 256960
+net.ipv4.tcp_timestamps = 0
 net.ipv4.tcp_keepalive_intvl = 30
-net.ipv4.tcp_max_orphans = 819200
-net.ipv4.tcp_max_syn_backlog = 20480
-net.ipv4.tcp_max_tw_buckets = 1440000
-net.ipv4.tcp_mem = 65536 1048576 33554432
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_fin_timeout = 15
 # net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_notsent_lowat = 32768
-net.ipv4.tcp_retries1 = 3
-net.ipv4.tcp_retries2 = 5
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_dsack = 1
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 30
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 30
+# net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_sack = 0
 net.ipv4.tcp_slow_start_after_idle = 0
 net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_adv_win_scale = 0
 net.ipv4.tcp_ecn = 0
-net.ipv4.tcp_ecn_fallback = 1
+net.ipv4.tcp_max_syn_backlog = 1024
+net.ipv4.tcp_syn_retries = 6
+net.ipv4.tcp_synack_retries = 3
 net.ipv4.tcp_syncookies = 1
-net.ipv4.udp_mem = 65536 1048576 33554432
 net.ipv6.conf.all.disable_ipv6 = 0
 net.ipv6.conf.default.disable_ipv6 = 0
 net.ipv6.conf.lo.disable_ipv6 = 0
-net.unix.max_dgram_qlen = 256
-vm.min_free_kbytes = 65536
 net.ipv4.ip_forward = 1
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
@@ -386,9 +368,8 @@ net.ipv6.conf.all.accept_source_route = 0
 net.ipv6.conf.default.accept_source_route = 0
 net.ipv6.conf.all.accept_ra = 0
 net.ipv6.conf.default.accept_ra = 0
-net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.tcp_congestion_control = bbr
-net.core.default_qdisc = fq_codel
+net.ipv4.tcp_congestion_control = cubic
+# net.core.default_qdisc = fq_codel
 
 ################################################################
 ################################################################
